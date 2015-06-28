755c2b4900d4cafbd6e709ffebb723dc41978394
Wataru TOYA <watrty@gmail.com>
2015-06-28 19:51:43 +0900

    download Brewfile

--- init.sh
+++ init.sh
@@ -5,4 +5,6 @@ set -e
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap Homebrew/brewdler
+curl -O https://raw.githubusercontent.com/kga/dotfiles/master/Brewfile
 brew bundle
+rm Brewfile
