24c02669bcd927b17d62a4f0687837b8473e172b
Wataru TOYA <watrty@gmail.com>
2015-07-05 22:32:40 +0900

    補完

--- init.sh
+++ init.sh
@@ -8,3 +8,5 @@ brew tap Homebrew/brewdler
 curl -O https://raw.githubusercontent.com/kga/dotfiles/master/Brewfile
 brew bundle
 rm Brewfile
+
+ln -s $(brew --prefix)/Library/Contributions/brew_zsh_completion.zsh $(brew --prefix)/share/zsh/site-functions/_brew
