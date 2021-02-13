d9a4c032164b0af6d69b779dd149c9f6a85e9cbf
Wataru TOYA <watrty@gmail.com>
2021-02-13 15:55:42 +0900

    gcloud.zsh

---
+++ .zsh/gcloud.zsh
@@ -0,0 +1,4 @@
+export CLOUDSDK_PYTHON="/usr/local/opt/python@3.9/libexec/bin/python"
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+
