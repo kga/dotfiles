81d851021a0b03ae8bcb8e3950d51c1589453669
Wataru TOYA <watrty@gmail.com>
2021-06-27 12:45:06 +0900

    fzf opts

---
+++ .zsh/fzf.zsh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="--cycle --layout=reverse
+--color='query:yellow:regular'
+--color='pointer:blue'
+--color='hl+:red:bold'
+--color='hl:red:regular'
+--color='fg+:green:bold'"
