6665da9fbc1f9e20cecc1e737b19e0da6e8bd8e9
Wataru TOYA <watrty@gmail.com>
2022-08-16 19:06:52 +0900

    completion

--- .zsh/gcloud.zsh
+++ .zsh/gcloud.zsh
@@ -1,2 +1,3 @@
 export CLOUDSDK_PYTHON="$(brew --prefix)/bin/python3"
 source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
